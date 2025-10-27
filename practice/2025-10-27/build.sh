eec82ae191cca2af740c2f022dcaa6670c6719f2
gendloop <gendloop@163.com>
2025-10-27 22:02:43 +0800

    chore: update

---
+++ practice/2025-10-27/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+#@brief  测试 RUN
+#@date   2025-10-27
+#@author gendloop
+
+docker build -t run:v1.0.0 .
+docker image ls run
+docker run --rm run:v1.0.0 
+docker image rm run:v1.0.0
---
+++ practice/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+#@brief  创建一个新目录
+#@author gendloop
+
+date=$(date '+%Y-%m-%d')
+mkdir $date
+echo "Folder '$date' has been created."
